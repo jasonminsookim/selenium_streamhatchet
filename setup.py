81e1cf5c6259a7ab89b15adb1c669aa7a974e3ac
Jason Kim <jasonkim@Jasons-MacBook-Pro-6.local>
2019-07-02 17:04:26 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A web browser automation script to gather data from streamhatchet.com',
+    author='Jason Minsoo Kim',
+    license='MIT',
+)
